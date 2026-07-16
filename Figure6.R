57cb2d948744ff1d6de9689670552e3d4904c070
huanyun1218924 <152439984+huanyun1218924@users.noreply.github.com>
2026-07-16 11:41:44 -0400

    Figure6.R

--- Figure6.R
+++ Figure6.R
@@ -1,37 +1,17 @@
 #load packages
 library(data.table)
-library(readxl)
 library(dplyr)
-library(tidyr)
-library(vegan)
-library(ape)
-library(ggplot2)
-library(ggvenn)
-library(ggalt)
-library(showtext)
-library(gridExtra)
-library(patchwork)
-library(nlme)
-library(emmeans)
-library(cowplot)
-library(openxlsx)
-library(MESS)
-library(PKNCA)
-library(pracma)
-library(lme4)
-library(lmerTest)
-library(UpSetR)
-library(ggsci)
+library(PKNCA)       #non-compartmental model kinetic analysis
 
-#define function to calculate NCA parameters
-ncafunc <- function(df,food) {
+#define function to calculate kinetic parameters for plasma metabolites
+ncafunc <- function(plasma_sample,food) {
   
   results = data.frame(group=NA,name=NA,Cmax=NA,Tmax=NA,AUC=NA,AUCinf=NA,Half=NA) #define a blank data frame to save results
-  data <- df[which(df$group==food),]
+  data <- plasma_sample[which(plasma_sample$group==food),]
   data$time <- as.numeric(data$time)
   data <- arrange(data,time)
   
-  for (i in var_group_time) {
+  for (i in metvar) {
     
     tryCatch({
       
@@ -41,7 +21,7 @@ ncafunc <- function(df,food) {
       d_conc = data[,c("ID","time","Dose","Concentration")]
       d_dose <- d_conc[d_conc$time == 0,]
       d_dose$time <- 0
-      id <- unique(intersect(d_conc$ID,d_dose$ID)) #get ID existed in both con and dose dataset
+      id <- unique(intersect(d_conc$ID,d_dose$ID))                                              #get ID existed in both con and dose dataset
       d_conc <- d_conc[which(d_conc$ID %in% id),]
       d_dose <- d_dose[which(d_dose$ID %in% id),]
       
@@ -52,7 +32,7 @@ ncafunc <- function(df,food) {
       res = pk.nca(obji)$result
       
       cmax_values <- res %>% filter((end == Inf) &(PPTESTCD == "cmax")) %>% select(PPORRES)
-      tmax_values <- res %>% filter((end == Inf) &(PPTESTCD == "tmax")) %>% select(PPORRES) + 1 ### tmax produced here is only relative to the start time
+      tmax_values <- res %>% filter((end == Inf) &(PPTESTCD == "tmax")) %>% select(PPORRES) + 1 #note:tmax produced here is only relative to the start time
       auc_values <- res %>% filter((end == 24) &(PPTESTCD == "auclast")) %>% select(PPORRES)
       aucinf_values <- res %>% filter((end == Inf) &(PPTESTCD == "aucinf.obs")) %>% select(PPORRES)
       halflife_values <- res %>% filter((end == Inf) &(PPTESTCD == "half.life")) %>% select(PPORRES)
@@ -77,15 +57,15 @@ ncafunc <- function(df,food) {
   return(results)
 }
 
-#define function to calculate NCA parameters for urinary metabolites
-ncafunc <- function(df,food) {
+#define function to calculate kinetic parameters for urinary metabolites
+ncafunc2 <- function(urine_sample,food) {
   
   results = data.frame(group=NA,name=NA,Cmax=NA,Tmax=NA,AUC=NA,AUCinf=NA,Half=NA) #define a blank data frame to save results
-  data <- df[which(df$group==food),]
+  data <- urine_sample[which(urine_sample$group==food),]
   data$time <- as.numeric(data$time)
   data <- arrange(data,time)
   
-  for (i in var_group_time) {
+  for (i in metvar) {
     
     tryCatch({
       
@@ -131,42 +111,83 @@ ncafunc <- function(df,food) {
   return(results)
 }
 
+#--------------------------------------------------------------------------------------------
+#
+#           part 1: calculate kinetic parameters of annotated plasma metabolites 
+#
+#--------------------------------------------------------------------------------------------
 #read data
-load("Res_no_filtering.Rdata")
+load("plasma_sample.RData")
 
-use <- unique(c(bf_all_new,ck_all_new,ch_all_new,yg_all_new,cr_all_new,ww_all_new,ot_all_new,pt_all_new))
-
-var_group_time <- use
-
-df$time <- as.numeric(df$time)
-
-table(df$time)
+#transform the timepoint data into numeric type for analysis
+plasma_sample$time <- as.numeric(plasma_sample$time)
+table(plasma_sample$time)
 #1   2   3   4   5   6   7   8   9  10 
 #116 118 113 117 110 114 108 112 103 114 
 
-df$time <- df$time-1
-table(df$time)
+plasma_sample$time <- plasma_sample$time-1
+table(plasma_sample$time)
 #0   1   2   3   4   5   6   7   8   9 
 #116 118 113 117 110 114 108 112 103 114
 
-df$time <- gsub("9","24",df$time)
-table(df$time)
+plasma_sample$time <- gsub("9","24",plasma_sample$time)
+plasma_sample$time <- as.numeric(plasma_sample$time) 
+table(plasma_sample$time)
+#0   1   2   3   4   5   6   7   8  24 
+#116 118 113 117 110 114 108 112 103 114
 
-df$time <- as.numeric(df$time)
+#calculate half, cmax, tmax across each metabolites by food
+pk_bf <- ncafunc(plasma_sample,food="Beef") 
+pk_cs <- ncafunc(plasma_sample,food="Cheese")
+pk_ck <- ncafunc(plasma_sample,food="Chicken")
+pk_yg <- ncafunc(plasma_sample,food="Yogurt")
+pk_cn <- ncafunc(plasma_sample,food="Corn")
+pk_ot <- ncafunc(plasma_sample,food="Oats")
+pk_pt <- ncafunc(plasma_sample,food="Potato")
+pk_bd <- ncafunc(plasma_sample,food="Whole Wheat Bread")
 
-#calculate half, cmax, tmax across different food groups and metabolites
-pk_bf <- ncafunc(df,food="Beef") 
-pk_cs <- ncafunc(df,food="Cheese")
-pk_ck <- ncafunc(df,food="Chicken")
-pk_yg <- ncafunc(df,food="Yogurt")
-pk_cn <- ncafunc(df,food="Corn")
-pk_ot <- ncafunc(df,food="Oats")
-pk_pt <- ncafunc(df,food="Potato")
-pk_bd <- ncafunc(df,food="Whole Wheat Bread")
+#save results
+save(pk_bf,pk_ck,pk_ch,pk_yg,pk_cn,pk_ot,pk_pt,pk_bd,file="Results_Plasma_NCA.RData")
 
-res_pk <- rbind(pk_bf[which(pk_bf$name %in% bf_all_new),],pk_cs[which(pk_bf$name %in% ch_all_new),],
-                pk_ck[which(pk_bf$name %in% ck_all_new),],pk_cn[which(pk_bf$name %in% cr_all_new),],
-                pk_ot[which(pk_bf$name %in% ot_all_new),],pk_pt[which(pk_bf$name %in% pt_all_new),],
-                pk_bd[which(pk_bf$name %in% ww_all_new),],pk_yg[which(pk_bf$name %in% yg_all_new),]) %>% as.data.frame()
+#--------------------------------------------------------------------------------------------
+#
+#           part 2: calculate kinetic parameters of annotated urinary metabolites 
+#
+#--------------------------------------------------------------------------------------------
+#read data
+load("urine_sample.RData")
 
-write.csv(res_pk, file="Res_PK_Plasma_Unfiltered.csv")
+#transform the timepoint data into numeric type for analysis
+urine_sample$time <- as.numeric(urine_sample$time)
+table(urine_sample$time)
+#1   2   3   4   5   6 
+#114 114 107 110 105 112 
+
+urine_sample$time <- urine_sample$time-1
+table(urine_sample$time)
+#0   1   2   3   4   5 
+#114 114 107 110 105 112 
+
+urine_sample$time <- gsub("4","8",urine_sample$time)
+urine_sample$time <- gsub("3","6",urine_sample$time)
+urine_sample$time <- gsub("2","4",urine_sample$time)
+urine_sample$time <- gsub("1","2",urine_sample$time)
+urine_sample$time <- gsub("5","24",urine_sample$time)
+
+urine_sample$time <- as.numeric(urine_sample$time)
+table(urine_sample$time)
+#0   2   4   6   8  24 
+#114 114 107 110 105 112 
+
+#calculate half, cmax, tmax across each metabolites by food
+pk_bf <- ncafunc2(urine_sample,food="Beef") 
+pk_cs <- ncafunc2(urine_sample,food="Cheese")
+pk_ck <- ncafunc2(urine_sample,food="Chicken")
+pk_yg <- ncafunc2(urine_sample,food="Yogurt")
+pk_cn <- ncafunc2(urine_sample,food="Corn")
+pk_ot <- ncafunc2(urine_sample,food="Oats")
+pk_pt <- ncafunc2(urine_sample,food="Potato")
+pk_bd <- ncafunc2(urine_sample,food="Whole Wheat Bread")
+
+#save results
+save(pk_bf,pk_ck,pk_ch,pk_yg,pk_cn,pk_ot,pk_pt,pk_bd,file="Results_Urine_NCA.RData")
