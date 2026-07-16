ad3c5b53219e1bedf54575353762adac7996615d
huanyun1218924 <152439984+huanyun1218924@users.noreply.github.com>
2026-07-16 11:06:22 -0400

    Figure3.R

--- Figure3.R
+++ Figure3.R
@@ -10,15 +10,28 @@ library(ggvenn)
 library(ggalt)
 library(gridExtra)
 library(patchwork)
-library(lme4)
+library(lme4)       #pacakge used for linear mixed model!
 library(emmeans)
 
 #read data
-load("BFI_for_use_no_filtering.RData")
-load("Processed_PK_Metabolome_No_Filtering.RData")
+load("plasma_sample.RData")
 
-metvar <- pla.anno$uID
+#check data
+dim(plasma_sample)
+#1125 995
 
+#check covariates
+covar
+
+#check metabolites
+metvar
+
+#check group and time
+table(plasma_sample$group)
+
+table(plasma_sample$time)
+
+#run linear mixed model to identify in response to food intake, adjusted for age, sex,race, and BMI
 results <- list()  
 results_l <- list()
 results_b <- list()
@@ -26,19 +39,19 @@ results_p <- list()
 results_e <- list()
 results_c <- list()
 
-for (i in 1:1000) {
+for (i in 1:length(metvar) {
   
   metabolite <- metvar[i]
-  data <- df[covar]
-  data$Concentration <- as.numeric(df[[metabolite]])  
+  data <- plasma_sample[covar]
+  data$Concentration <- as.numeric(plasma_sample[[metabolite]])  
   data$group <- as.factor(data$group)
   data$time <- as.factor(data$time)
   
-  # Run linear mixed model
+  #run linear mixed model
   fit1 <- lmer(log(Concentration) ~ group*time + ips_age + ps_sex + race + ips_bmi + (1 | ID), data = data)
   fit2 <- lmer(log(Concentration) ~ group + time + ips_age + ps_sex + race + ips_bmi + (1 | ID), data = data)
   
-  # Store the results
+  #save the results
   results[[i]] <- summary(fit1)
   results_l[[i]] <- anova(fit1, fit2)[2,"Pr(>Chisq)"]
   results_b[[i]] <- summary(emmeans(fit1, pairwise ~ group | time))$emmeans
@@ -48,4 +61,5 @@ for (i in 1:1000) {
   
 }
 
+#save results
 save(results,results_b,results_p,results_e,results_c,results_l,file="Res_LMM_Plasma.RData")
